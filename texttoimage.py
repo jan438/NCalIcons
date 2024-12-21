7b6a7282995399c588dc1812588f4f5722438d60
jan438 <jan438@users.noreply.github.com>
2024-12-21 11:08:12 +0100

    text change added

--- texttoimage.py
+++ texttoimage.py
@@ -17,12 +17,12 @@ img = Image.open('Icons/Ent1.png')
 I1 = ImageDraw.Draw(img)
 
 if sys.platform[0] == 'l': 
-    myFont = ImageFont.truetype('C:/Fonts/Arial.ttf', 200)
+    myFont = ImageFont.truetype('C:/Fonts/Arial.ttf', 75)
 if sys.platform[0] == 'w':
-    myFont = ImageFont.truetype('C:/Users/janbo/Fonts/Arial.ttf', 200)
+    myFont = ImageFont.truetype('C:/Users/janbo/Fonts/Arial.ttf', 75)
  
 # Add Text to an image
-I1.text((10, 10), "Nice Car", font=myFont, fill =(255, 0, 0))
+I1.text((60, 600), "164", font=myFont, fill = (200, 200, 200))
  
 # Save the edited image
 img.save("Ent1s.png")
