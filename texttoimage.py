884068872439f03213ce5744953869c51b7c085c
jan438 <jan438@users.noreply.github.com>
2024-12-21 10:18:34 +0100

    if windows clause added

--- texttoimage.py
+++ texttoimage.py
@@ -18,6 +18,8 @@ I1 = ImageDraw.Draw(img)
 
 if sys.platform[0] == 'l': 
     myFont = ImageFont.truetype('C:/Fonts/Arial.ttf', 200)
+if sys.platform[0] == 'w':
+    myFont = ImageFont.truetyoe('C:/Users/janbo/Fonts/Arial.ttf', 200)
  
 # Add Text to an image
 I1.text((10, 10), "Nice Car", font=myFont, fill =(255, 0, 0))
