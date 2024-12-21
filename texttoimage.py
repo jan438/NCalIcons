16be00a2e547437b9a507f5cc519d61ea054107f
jan438 <jan438@users.noreply.github.com>
2024-12-21 11:41:56 +0100

    Folder indexes added

--- texttoimage.py
+++ texttoimage.py
@@ -13,10 +13,8 @@ os.chdir(path)
 fontsize = 75
 fontcolor = (200, 200, 200) 
  
-# Open an Image
 img = Image.open('Icons/Ent1.png')
  
-# Call draw Method to add 2D graphics in an image
 I1 = ImageDraw.Draw(img)
 
 if sys.platform[0] == 'l': 
@@ -24,10 +22,8 @@ if sys.platform[0] == 'l':
 if sys.platform[0] == 'w':
     myFont = ImageFont.truetype('C:/Users/janbo/Fonts/Arial.ttf', fontsize)
  
-# Add Text to an image
 I1.text((60, 600), "164", font=myFont, fill = fontcolor)
  
-# Save the edited image
-img.save("Ent1s.png")
+img.save("Indexes/Ent1s.png")
 
 key = input("Wait")
