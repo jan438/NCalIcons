f5cff1538889bee536bb96e7fdd666b296ced914
jan438 <jan438@users.noreply.github.com>
2024-12-22 16:15:26 +0100

    Error corrected

--- texttoimage.py
+++ texttoimage.py
@@ -5,11 +5,11 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 def merge(im1: Image.Image, im2: Image.Image):
-    w = max(im1.size[1], im2.size[1])
-    h = im1.size[0] + im2.size[0]
+    w = max(im1.size[0], im2.size[0])
+    h = im1.size[1] + im2.size[1]
     im = Image.new("RGBA", (w, h))
     im.paste(im1)
-    im.paste(im2, (0, im1.size[0]))
+    im.paste(im2, (0, im1.size[1]))
     return im
     
 if sys.platform[0] == 'l':
