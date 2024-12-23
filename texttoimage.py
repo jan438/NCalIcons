87796d6c0991196c087b18fa0e05058d0fb67e2b
jan438 <jan438@users.noreply.github.com>
2024-12-23 16:18:32 +0100

    code reorginized

--- texttoimage.py
+++ texttoimage.py
@@ -11,11 +11,14 @@ def merge(im1: Image.Image, im2: Image.Image):
     im.paste(im1)
     im.paste(im2, (0, im1.size[1]))
     return im
-    
+
+fontsize = 60    
 if sys.platform[0] == 'l':
     path = '/home/jan/git/NCalIcons'
+    myFont = ImageFont.truetype('/home/jan/Fonts/Arial.ttf', fontsize)
 if sys.platform[0] == 'w':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/NCalIcons"
+    myFont = ImageFont.truetype('C:/Users/janbo/Fonts/Arial.ttf', fontsize)
 os.chdir(path)
 numcols = 8
 numrows = 10
@@ -31,7 +34,6 @@ entindexes = [["263","225","264","264","264","265","164","176"],
               ["164","164","164","164","164","164","164","164"],
               ["164","164","164","164","164","164","164","164"]]
 
-fontsize = 60
 fontcolor = (0, 0, 0)
 startx = 100
 starty = 300
@@ -39,23 +41,14 @@ width = 1400
 height = 1536
 deltax = width / numcols
 deltay = height / numrows
- 
 img1 = Image.open('Icons/Ent1.png')
 box1 = (0, 0, 1537, 1100)
 img1 = img1.crop(box1)
 img2 = Image.open('Icons/Ent2.png')
 box2 = (0, 320, 1537, 1100)
 img2 = img2.crop(box2)
-
 img = merge(img1, img2)
- 
 I = ImageDraw.Draw(img)
-
-if sys.platform[0] == 'l': 
-    myFont = ImageFont.truetype('/home/jan/Fonts/Arial.ttf', fontsize)
-if sys.platform[0] == 'w':
-    myFont = ImageFont.truetype('C:/Users/janbo/Fonts/Arial.ttf', fontsize)
-
 posy = starty
 for i in range(numrows):
     posx = startx
